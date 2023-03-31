bd9d6fd7b0576b884e2f83e7188616c5c75ccd90
SantosUwU <94976072+SantosUwU@users.noreply.github.com>
2023-03-31 17:46:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 
-let cores = []
-let ctrlZ = []
-// let dinossauro = []
+// let cores = []
+let dinos = []
+let iDinoSelecionado = -1
 
-// function cadastrar(){
-//     var dino = {
-//         nome: ""
-//     }
-//     dino.nome = document.getElementById("divCriarDino").value
+function adicionarDino() {
+    document.getElementById('divCorCompleta').style.visibility = 'hidden'
+    document.getElementById('divFechou5').style.visibility = 'hidden'
+    document.getElementById('divFechouTodos').style.visibility = 'hidden'
+    document.getElementById('divResultado').style.background = "#212121"
+    document.getElementById('divResultado').innerHTML = ""
+    document.getElementById('divContainer').style.background = "#212121"
 
+    let dino = {
+        nome: prompt("Qual o nome do dino? "),
+        cores: criarVetordeCores(),
+        coresCompletas: [],
+        ctrlZ: []
+    }
 
-//     fila.push(dino)
-//     mostrar()
+    dinos.push(dino)
+    iDinoSelecionado = dinos.length - 1
+    atualizarSelect()
+    selectDinos.value = dinos[iDinoSelecionado].nome
+    atualizarCores()
+    atualizarCoresCompletas()
+}
+
+let selectDinos = document.getElementById('selectDinos')
+
+function atualizarSelect() {
+    selectDinos.innerHTML = ""
+    dinos.forEach(e => selectDinos.innerHTML += `<option value=${e.nome}>${e.nome}</option>`);
+
+    // selectDinos.innerHTML += `<option value='ola mundo' >ola dnv</option>`
+}
+
+function selecionarDino() {
+    for (i = 0; i < dinos.length; i++) {
+        if (dinos[i].nome == selectDinos.value) {
+            iDinoSelecionado = i
+            atualizarCores()
+            atualizarCoresCompletas()
+        }
+    }
     
-// }
 
-function criarVetorPrincipal() {
+
+}
+
+function criarVetordeCores() {
+    let cores = []
     for (i = 0; i <= 227; i++) {
         let cor = {
             regioes: [1, 1, 1, 1, 1, 0],
@@ -23,16 +57,18 @@ function criarVetorPrincipal() {
         }
         cores.push(cor)
     }
-
+    return cores
 }
 
 function atualizarCores() {
-    for (i = 1; i <= 227; i++) {
-        if (i <= 100 || i >= 201) {
-            document.getElementById('cor' + i).innerHTML = i + ' ' + nomesCores[i] + ' |'
-            for (j = 0; j <= 5; j++) {
-                document.getElementById('cor' + i).innerHTML += `${j}:${cores[i].regioes[j]}|`
+    if (iDinoSelecionado != -1) {
+        for (i = 1; i <= 227; i++) {
+            if (i <= 100 || i >= 201) {
+                document.getElementById('cor' + i).innerHTML = i + ' ' + nomesCores[i] + ' |'
+                for (j = 0; j <= 5; j++) {
+                    document.getElementById('cor' + i).innerHTML += `${j}:${dinos[iDinoSelecionado].cores[i].regioes[j]}|`
 
+                }
             }
         }
     }
@@ -41,13 +77,17 @@ function atualizarCores() {
 divCores = document.getElementById("divCores")
 
 function atualizarCoresCompletas() {
-    let coresCompletas = []
-    for (i = 1; i < cores.length; i++) {
-        if (cores[i].completa == true) {
-            coresCompletas.push(i)
+    if (iDinoSelecionado != -1) {
+        dinos[iDinoSelecionado].coresCompletas = []
+        if (iDinoSelecionado != -1) {
+            for (i = 1; i < dinos[iDinoSelecionado].cores.length; i++) {
+                if (dinos[iDinoSelecionado].cores[i].completa == true) {
+                    dinos[iDinoSelecionado].coresCompletas.push(i)
+                }
+            }
         }
+        document.getElementById('divCoresCompletas').innerHTML = "Lista de cores completas: " + dinos[iDinoSelecionado].coresCompletas
     }
-    document.getElementById('divCoresCompletas').innerHTML = "Lista de cores completas: " + coresCompletas
 }
 
 function criarTabelaDeCores() {
@@ -94,14 +134,14 @@ function enterReg(e) {
 
 function desfazer(e) {
     if (e.ctrlKey && e.key == 'z') {
-        if (ctrlZ.length > 0) {
-            let texto = `Desfazer [${ctrlZ[ctrlZ.length - 1].corInserida}:${ctrlZ[ctrlZ.length - 1].regiaoInserida}] ?`
-            if (ctrlZ[ctrlZ.length - 1].completouInserida) texto += `\nEsta ação vai descompletar a cor [${ctrlZ[ctrlZ.length - 1].corInserida}]`
+        if (dinos[iDinoSelecionado].ctrlZ.length > 0) {
+            let texto = `Desfazer [${dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].corInserida}:${dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].regiaoInserida}] ?`
+            if (dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].completouInserida) texto += `\nEsta ação vai descompletar a cor [${dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].corInserida}]`
             if (confirm(texto)) {
-                if (cores[ctrlZ[ctrlZ.length - 1].corInserida].regioes[ctrlZ[ctrlZ.length - 1].regiaoInserida] != 0) {
-                    cores[ctrlZ[ctrlZ.length - 1].corInserida].regioes[ctrlZ[ctrlZ.length - 1].regiaoInserida]--
-                    cores[ctrlZ[ctrlZ.length - 1].corInserida].completa = false
-                    ctrlZ.pop()
+                if (dinos[iDinoSelecionado].cores[dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].corInserida].regioes[dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].regiaoInserida] != 0) {
+                    dinos[iDinoSelecionado].cores[dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].corInserida].regioes[dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].regiaoInserida]--
+                    dinos[iDinoSelecionado].cores[dinos[iDinoSelecionado].ctrlZ[dinos[iDinoSelecionado].ctrlZ.length - 1].corInserida].completa = false
+                    dinos[iDinoSelecionado].ctrlZ.pop()
                     atualizarCores()
                     document.getElementById('divResultado').style.background = "#212121"
                     document.getElementById('divResultado').innerHTML = ""
@@ -109,7 +149,7 @@ function desfazer(e) {
                     document.getElementById('divCorCompleta').style.visibility = "hidden"
                 } else {
                     alert("Nada feito")
-                    ctrlZ = []
+                    dinos[iDinoSelecionado].ctrlZ = []
                     document.getElementById('divResultado').style.background = "#212121"
                     document.getElementById('divResultado').innerHTML = ""
                     document.getElementById('divContainer').style.background = "#212121"
@@ -125,88 +165,89 @@ function inserirCor() {
     document.getElementById('divFechouTodos').style.visibility = 'hidden'
     let corLida = (document.getElementById('inpCor').value)
     let regLida = (document.getElementById('inpReg').value)
-    if (corLida == "" || regLida == "") {
-        alert("digita algo ai menor")
-        document.getElementById('inpCor').focus()
-    } else {
-        corLida = Number(corLida)
-        regLida = Number(regLida)
+    if (iDinoSelecionado != -1) {
+        if (corLida == "" || regLida == "") {
+            alert("digita algo ai menor")
+            document.getElementById('inpCor').focus()
+        } else {
+            corLida = Number(corLida)
+            regLida = Number(regLida)
 
-        if ((regLida >= 0 && regLida <= 5)) {
+            if ((regLida >= 0 && regLida <= 5)) {
 
-            // console.log(corLida);
-            if (cores[corLida].regioes[regLida] == 0) {
-                document.getElementById('divResultado').style.background = "green"
-                cores[corLida].regioes[regLida] = cores[corLida].regioes[regLida] + 1
-                // console.log(cores[corLida].regioes);
-                let completouAgora = verificarSeCompletou(corLida)
-                registrarInsercao(corLida, regLida, completouAgora)
-            } else {
-                if (cores[corLida].regioes[regLida] < 5) {
-                    document.getElementById('divResultado').style.background = "yellow"
-                    cores[corLida].regioes[regLida]++
-                    if (cores[corLida].regioes[regLida] == 5) { document.getElementById('divFechou5').style.visibility = 'visible' }
-                    if (fechouTodos(corLida)) { document.getElementById('divFechouTodos').style.visibility = 'visible' }
+                if (dinos[iDinoSelecionado].cores[corLida].regioes[regLida] == 0) {
+                    document.getElementById('divResultado').style.background = "green"
+                    dinos[iDinoSelecionado].cores[corLida].regioes[regLida] = dinos[iDinoSelecionado].cores[corLida].regioes[regLida] + 1
+                    // console.log(cores[corLida].regioes);
                     let completouAgora = verificarSeCompletou(corLida)
                     registrarInsercao(corLida, regLida, completouAgora)
                 } else {
-                    document.getElementById('divResultado').style.background = "red"
-                }
-            }
-
-            function verificarSeCompletou(cor) {
-                // console.log(cores[cor].completa)
-                if (cores[cor].completa == false) {
-                    // console.log(cores[cor].regioes.length)
-                    for (i = 0; i < cores[cor].regioes.length; i++) {
-                        if (cores[cor].regioes[i] == 0) {
-                            return false
-                        }
+                    if (dinos[iDinoSelecionado].cores[corLida].regioes[regLida] < 5) {
+                        document.getElementById('divResultado').style.background = "yellow"
+                        dinos[iDinoSelecionado].cores[corLida].regioes[regLida]++
+                        if (dinos[iDinoSelecionado].cores[corLida].regioes[regLida] == 5) { document.getElementById('divFechou5').style.visibility = 'visible' }
+                        if (fechouTodos(corLida)) { document.getElementById('divFechouTodos').style.visibility = 'visible' }
+                        let completouAgora = verificarSeCompletou(corLida)
+                        registrarInsercao(corLida, regLida, completouAgora)
+                    } else {
+                        document.getElementById('divResultado').style.background = "red"
                     }
-                    cores[cor].completa = true
-                    document.getElementById('divCorCompleta').style.visibility = "visible"
-                    return true
-                } else {
-                    return false
                 }
-            }
 
-            function registrarInsercao(cor, regiao, completouAgora) {
-                let insercao = {
-                    corInserida: cor,
-                    regiaoInserida: regiao,
-                    completouInserida: completouAgora
+                function verificarSeCompletou(cor) {
+                    // console.log(cores[cor].completa)
+                    if (dinos[iDinoSelecionado].cores[cor].completa == false) {
+                        // console.log(cores[cor].regioes.length)
+                        for (i = 0; i < dinos[iDinoSelecionado].cores[cor].regioes.length; i++) {
+                            if (dinos[iDinoSelecionado].cores[cor].regioes[i] == 0) {
+                                return false
+                            }
+                        }
+                        dinos[iDinoSelecionado].cores[cor].completa = true
+                        document.getElementById('divCorCompleta').style.visibility = "visible"
+                        return true
+                    } else {
+                        return false
+                    }
                 }
-                ctrlZ.push(insercao)
-                // console.log(insercao)
+
+                function registrarInsercao(cor, regiao, completouAgora) {
+                    let insercao = {
+                        corInserida: cor,
+                        regiaoInserida: regiao,
+                        completouInserida: completouAgora
+                    }
+                    dinos[iDinoSelecionado].ctrlZ.push(insercao)
+                    // console.log(insercao)
+                }
+
+
+                atualizarCores()
+                document.getElementById('divContainer').style.background = hexCores[corLida]
+
+                document.getElementById('inpCor').focus()
+
+                //*** teste
+                // document.getElementById('divResultado').innerHTML = '['+ + ':'
+                // document.getElementById('divResultado').innerHTML +=  + ']<br>'
+
+                document.getElementById('divResultado').innerHTML = `<div>[${document.getElementById('inpCor').value}:${document.getElementById('inpReg').value}]</div>
+    <div>Q:${dinos[iDinoSelecionado].cores[corLida].regioes[regLida]}</div>`
+
+
+                //*** /teste
+                document.getElementById('inpCor').value = ''
+                document.getElementById('inpReg').value = ''
+                // if (!dinos[iDinoSelecionado].cores[corLida].regioes.includes(0)) {
+                //     // alert("ola")
+
+                // }
+            } else {
+                alert(regLida + " É um valor de ID incorreto para 'Região'")
+                document.getElementById('inpReg').value = ''
             }
-
-
-            atualizarCores()
-            document.getElementById('divContainer').style.background = hexCores[corLida]
-
-            document.getElementById('inpCor').focus()
-
-            //*** teste
-            // document.getElementById('divResultado').innerHTML = '['+ + ':'
-            // document.getElementById('divResultado').innerHTML +=  + ']<br>'
-
-            document.getElementById('divResultado').innerHTML = `<div>[${document.getElementById('inpCor').value}:${document.getElementById('inpReg').value}]</div>
-    <div>Q:${cores[corLida].regioes[regLida]}</div>`
-
-
-            //*** /teste
-            document.getElementById('inpCor').value = ''
-            document.getElementById('inpReg').value = ''
-            if (!cores[corLida].regioes.includes(0)) {
-                // alert("ola")
-
-            }
-        } else {
-            alert(regLida + " É um valor de ID incorreto para 'Região'")
-            document.getElementById('inpReg').value = ''
         }
-    }
+    } else { alert('Selecione o dino') }
 }
 
 function exportar() {
@@ -226,7 +267,7 @@ function importar() {
 
 function fechouTodos(cor) {
     for (i = 0; i <= 5; i++) {
-        if (cores[cor].regioes[i] != 5) {
+        if (dinos[iDinoSelecionado].cores[cor].regioes[i] != 5) {
             return false
         }
     }
@@ -236,10 +277,13 @@ function fechouTodos(cor) {
 
 
 
+
+
+
 criarVetorHexCores()
 criarVetorNomesCor()
 criarTabelaDeCores()
-criarVetorPrincipal()
+criarVetordeCores()
 atualizarCores()
 document.getElementById('inpCor').focus()
 
